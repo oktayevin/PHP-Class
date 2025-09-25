e47af9542744471075e53954e6184c2c4749a899
Oktay Evin <63397576+oktayevin@users.noreply.github.com>
2025-09-25 13:23:58 +0000

    Until Fourth Exercise

---
+++ fourth.php
@@ -0,0 +1,14 @@
+<?php
+// fourth.php
+?>
+"<!DOCTYPE html>\n"
+<html lang="en">\n"
+<head>\n"
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fourth Exercise</title>
+</head>
+<body>
+    
+</body>
+</html>
